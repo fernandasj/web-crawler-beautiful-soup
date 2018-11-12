1808df35b0de10ca775f652055c602e29342caba
Fernanda Vieira <fernanda.silva_sj@hotmail.com>
2018-11-12 08:39:56 -0300

    Initial commit

--- WebCrawller.py
+++ WebCrawller.py
@@ -5,6 +5,13 @@ from bs4 import BeautifulSoup as bs
 pagina = requests.get('https://www.bbc.com/portuguese')
 conteudo = bs(pagina.content, 'html.parser')
 noticias = conteudo.findAll('span', {'class': 'title-link__title-text'})
+titulo = conteudo.find('title')
+
+print('\n')
+print('___________________________________')
+print(titulo.string)
+print('___________________________________')
+print('\n')
 
 for noticia in noticias:
     print('===================================================================')
