a4512a9ff22873e1a645497bd6aa0a317e394c61
Fernanda Vieira <fernanda.silva_sj@hotmail.com>
2018-12-06 19:39:43 -0300

    Escrevendo noticias em arquivo .txt

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,23 +1,46 @@
 #! /usr/bin/env python3
-import this
+
 import requests
 from bs4 import BeautifulSoup as bs
 
-pagina = requests.get('https://www.bbc.com/portuguese')
-conteudo = bs(pagina.content, 'html.parser')
-noticias = conteudo.findAll('span', {'class': 'title-link__title-text'})
-titulo = conteudo.find('title')
 
-print('\n')
-print('SITE:')
-print('___________________________________')
-print(titulo.string)
-print('___________________________________')
-print('\n')
+def writeTitle(titulo, arquivo):
+    with open(arquivo, "w") as file:
+        file.write("___________________________________" + "\n")
+        file.write(titulo.string.encode("UTF-8") + "\n")
+        file.write("___________________________________" + "\n")
 
-for noticia in noticias:
-    print('NOTICIA:')
-    print('===================================================================')
-    print(noticia.string)
-    print('===================================================================')
+
+def writeNew(noticias, arquivo):
+    with open(arquivo, "a") as file:
+        for noticia in noticias:
+            file.write("NOTICIA:" + "\n")
+            file.write("===================================================================" + "\n")
+            file.write(noticia.string.encode("UTF-8") + "\n")
+            file.write("===================================================================" + "\n")
+
+if __name__ == "__main__":
+
+    arquivo = "noticias-bbc.txt"
+
+    pagina = requests.get('https://www.bbc.com/portuguese')
+    conteudo = bs(pagina.content, 'html.parser')
+    noticias = conteudo.findAll('span', {'class': 'title-link__title-text'})
+    titulo = conteudo.find('title')
+
+    writeTitle(titulo, arquivo)
+    writeNew(noticias, arquivo)
+
     print('\n')
+    print('SITE:')
+    print('___________________________________')
+    print(titulo.string)
+    print('___________________________________')
+    print('\n')
+
+    for noticia in noticias:
+        print('NOTICIA:')
+        print('===================================================================')
+        print(noticia.string)
+        print('===================================================================')
+        print('\n')
